77ba763248a961d76ed5c42db82eb402534f121d
byesol <bsj5967@naver.com>
2025-03-10 22:24:04 +0900

    feat: 덱에서 카드 드로우하는 기능 작성

--- src/main/java/Deck.java
+++ src/main/java/Deck.java
@@ -1,14 +1,20 @@
+import java.util.ArrayDeque;
+import java.util.Deque;
 import java.util.List;
 
 public class Deck {
 
-    private final List<Card> cards;
+    private final Deque<Card> cards;
 
     public Deck(List<Card> cards) {
-        this.cards = cards;
+        this.cards = new ArrayDeque<Card>(cards);
     }
 
     public int size() {
         return cards.size();
+    }
+
+    public Card drawCard() {
+        return cards.pollFirst();
     }
 }
--- src/test/java/DeckTest.java
+++ src/test/java/DeckTest.java
@@ -8,7 +8,7 @@ public class DeckTest {
 
     @Test
     @DisplayName("카드로 이루어진 덱이 있다. ")
-    void testMethodNameHere() {
+    void deckTest() {
         // given
         Card card1 = new Card(CardType.ONE, CardShape.SPADE);
         Card card2 = new Card(CardType.ONE, CardShape.SPADE);
@@ -18,5 +18,21 @@ public class DeckTest {
 
         // then
         assertThat(deck.size()).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("덱에서 카드를 드로우할 수 있다.")
+    void drawCardTest() {
+        Deck deck = DeckInitializer.generateDeck();
+        Card card = deck.drawCard();
+        assertThat(card).isNotNull();
+    }
+
+    @Test
+    @DisplayName("덱에서 카드를 드로우하면 덱의 카드가 줄어든다.")
+    void drawDeckTest() {
+        Deck deck = DeckInitializer.generateDeck();
+        Card card = deck.drawCard();
+        assertThat(deck.size()).isEqualTo(51);
     }
 }
