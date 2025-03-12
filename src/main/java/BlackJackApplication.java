a4da3ad6e9b79c772d1ff9759da48232dc795831
byesol <bsj5967@naver.com>
2025-03-12 13:55:12 +0900

    feat: 게임 result 도메인 기능 작성

---
+++ src/main/java/BlackJackApplication.java
@@ -0,0 +1,7 @@
+public class BlackJackApplication {
+
+    public static void main(String[] args) {
+        InputView inputView = new InputView();
+
+    }
+}
---
+++ src/main/java/InputView.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class InputView {
+
+    private static Scanner scanner = new Scanner(System.in);
+
+    public String inputPlayers() {
+        System.out.println(String.format(""));
+    }
+}
