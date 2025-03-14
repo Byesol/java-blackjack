d74587ced60c307ee4c8eb2bae116f6a28e4ae70
byesol <bsj5967@naver.com>
2025-03-14 17:26:56 +0900

    feat: InputView

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -2,9 +2,10 @@ import java.util.Scanner;
 
 public class InputView {
 
-    private static Scanner scanner = new Scanner(System.in);
+    private static Scanner SCANNER = new Scanner(System.in);
 
     public String inputPlayers() {
-        System.out.println(String.format(""));
+        System.out.println(String.format("플레이어 입력해라"));
+        return SCANNER.nextLine();
     }
 }
